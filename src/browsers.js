5f9198d74aa553bdfa8023dd5d119396b5965638
Ludovic Muller <ludovicmuller1@gmail.com>
2021-09-23 19:01:05 +0200

    generate static results

--- src/browsers.js
+++ src/browsers.js
@@ -1,27 +1,27 @@
-const { chromium, firefox } = require('playwright');
+const { chromium, firefox } = require("playwright");
 
 const browsers = [
   {
-    name: 'chromium',
+    name: "chromium",
     browser: chromium,
     options: {
       headless: true,
       args: [
-        '--use-fake-device-for-media-stream',
-        '--use-fake-ui-for-media-stream'
+        "--use-fake-device-for-media-stream",
+        "--use-fake-ui-for-media-stream",
       ],
     },
-    permissions: ['camera', 'microphone'],
+    permissions: ["camera", "microphone"],
   },
   {
-    name: 'firefox',
+    name: "firefox",
     browser: firefox,
     options: {
       headless: true,
       firefoxUserPrefs: {
-        'media.navigator.streams.fake': true,
-        'media.navigator.permission.disabled': true,
-      }
+        "media.navigator.streams.fake": true,
+        "media.navigator.permission.disabled": true,
+      },
     },
     permissions: [],
   },
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-const browsers = require('./browsers');
+const fs = require("fs");
+const browsers = require("./browsers");
 
 const waitSeconds = async (seconds) => {
-  await new Promise(r => setTimeout(r, seconds * 1000));
-}
+  await new Promise((r) => setTimeout(r, seconds * 1000));
+};
 
-const url = 'https://webrtc-local-test.ludovicm67.fr/';
+const url = "https://webrtc-local-test.ludovicm67.fr/";
+const generatedDirectory = "./public";
 
 const runBrowser = async (browser) => {
   const instance = await browser.browser.launch(browser.options);
-  const page = await instance.newPage({permissions: browser.permissions});
+  const page = await instance.newPage({ permissions: browser.permissions });
   await page.goto(url);
   await waitSeconds(3);
   const stats = await page.evaluate(() => JSON.stringify(getStatsProperties()));
@@ -20,9 +22,54 @@ const runBrowser = async (browser) => {
     version,
     stats,
   };
-}
+};
+
+const deviceKeyFromResult = (r) => {
+  return `${r.name}${r.version}`
+    .toLocaleLowerCase()
+    .replace(/ /g, "")
+    .replace(/\./g, "");
+};
+
+const deviceFromResult = (r) => {
+  return {
+    browser: {
+      name: r.name,
+      version: r.version,
+    },
+  };
+};
 
 (async () => {
-  const results = await Promise.all(browsers.map(browser => runBrowser(browser)));
+  const results = await Promise.all(
+    browsers.map((browser) => runBrowser(browser))
+  );
+
+  // create devices list
+  const devices = {};
+  const data = {};
+  results.forEach((r) => {
+    const key = deviceKeyFromResult(r);
+    devices[`${key}`] = deviceFromResult(r);
+    data[`${key}`] = JSON.parse(r.stats);
+  });
+
+  // create generated files
+  if (!fs.existsSync(`${generatedDirectory}/data`)) {
+    fs.mkdirSync(`${generatedDirectory}/data`, { recursive: true });
+  }
+  fs.createReadStream("index.html").pipe(
+    fs.createWriteStream(`${generatedDirectory}/index.html`)
+  );
+  const devicesStream = fs.createWriteStream(
+    `${generatedDirectory}/data/devices.js`
+  );
+  const dataStream = fs.createWriteStream(`${generatedDirectory}/data/data.js`);
+  devicesStream.write(`const devices = ${JSON.stringify(devices)};`);
+  dataStream.write(`const data = ${JSON.stringify(data)};`);
+  devicesStream.end();
+  dataStream.end();
+
+  // print results in the console
   console.log(results);
 })();
