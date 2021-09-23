d54cfcd1098f8575c178977c053e34b8c5b7a789
Ludovic Muller <ludovicmuller1@gmail.com>
2021-09-23 18:31:03 +0200

    create file for browsers

---
+++ src/browsers.js
@@ -0,0 +1,30 @@
+const { chromium, firefox } = require('playwright');
+
+const browsers = [
+  {
+    name: 'chromium',
+    browser: chromium,
+    options: {
+      headless: true,
+      args: [
+        '--use-fake-device-for-media-stream',
+        '--use-fake-ui-for-media-stream'
+      ],
+    },
+    permissions: ['camera', 'microphone'],
+  },
+  {
+    name: 'firefox',
+    browser: firefox,
+    options: {
+      headless: true,
+      firefoxUserPrefs: {
+        'media.navigator.streams.fake': true,
+        'media.navigator.permission.disabled': true,
+      }
+    },
+    permissions: [],
+  },
+];
+
+module.exports = browsers;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { chromium, firefox } = require('playwright');
+const browsers = require('./browsers');
 
 const waitSeconds = async (seconds) => {
   await new Promise(r => setTimeout(r, seconds * 1000));
@@ -23,34 +23,6 @@ const runBrowser = async (browser) => {
 }
 
 (async () => {
-
-  const browsers = [
-    {
-      name: 'chromium',
-      browser: chromium,
-      options: {
-        headless: true,
-        args: [
-          '--use-fake-device-for-media-stream',
-          '--use-fake-ui-for-media-stream'
-        ],
-      },
-      permissions: ['camera', 'microphone'],
-    },
-    {
-      name: 'firefox',
-      browser: firefox,
-      options: {
-        headless: true,
-        firefoxUserPrefs: {
-          'media.navigator.streams.fake': true,
-          'media.navigator.permission.disabled': true,
-        }
-      },
-      permissions: [],
-    },
-  ];
-
   const results = await Promise.all(browsers.map(browser => runBrowser(browser)));
   console.log(results);
 })();
